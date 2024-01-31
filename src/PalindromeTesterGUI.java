b11fe9b0b3046dd657f14365e53eb5876feac6c9
Dr Gary Allen <g.allen@hud.ac.uk>
2024-01-31 13:14:43 +0000

    Oops - now added the GUI files

--- src/PalindromeTesterGUI.java
+++ src/PalindromeTesterGUI.java
@@ -1,2 +1,39 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class PalindromeTesterGUI {
+    private RecursivePalindromeSolver solver;
+    private JTextField inputTF;
+    private JLabel title;
+    private JButton checkButton;
+    private JLabel resultLabel;
+    private JPanel rootGUI;
+
+    public PalindromeTesterGUI() {
+        solver = new RecursivePalindromeSolver();
+        checkButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if (solver.isPalindrome(inputTF.getText()))
+                    resultLabel.setText("is a Palindrome       ");
+                else
+                    resultLabel.setText("is not a Palindrome   ");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                 UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        JFrame frame = new JFrame("PalindromeTesterGUI");
+        frame.setContentPane(new PalindromeTesterGUI().rootGUI);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,200);
+        frame.setVisible(true);
+    }
 }
